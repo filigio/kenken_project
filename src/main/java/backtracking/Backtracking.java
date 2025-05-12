412c2ac85e9dcc138c2389016b8889aedd4afc93
filigio <felice.dardis@gmail.com>
2025-05-12 14:12:02 +0200

    Struttura iniziale della classe Backtracking
    Costruttore con griglia, metodo solve() vuoto. Implementazione da completare nei passaggi successivi.

---
+++ src/main/java/backtracking/Backtracking.java
@@ -0,0 +1,16 @@
+package main.java.backtracking;
+
+import main.java.model.Grid;
+
+public class Backtracking {
+    private final Grid grid;
+
+    public Backtracking(Grid g) {
+        this.grid = g;
+    }
+
+    public int[][] solve() {
+        // Metodo ancora da implementare
+        return null;
+    }
+}
